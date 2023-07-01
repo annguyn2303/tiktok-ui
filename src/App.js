46e3080a4d69026841222736b6b1664fa2fd5d36
annguyn2303 <annguyn711@gmail.com>
2023-07-01 20:37:12 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { HeaderOnly } from '~/components/Layout'
+
 import { Fragment } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { publicRoutes } from '~/routes'
@@ -12,8 +14,16 @@ function App() {
       <div className="text-6xl">
         <Routes>
           {publicRoutes.map((route, index) => {
-            const Layout = route.layout === null ? Fragment : DefaultLayout;
             const Page = route.componet
+
+            let Layout = DefaultLayout
+
+            if (route.layout) {
+              Layout = route.layout
+            } else if (route.layout === null) {
+              Layout = Fragment
+            }
+
             return <Route key={index} path={route.path} element={
               <Layout>
                 <Page />
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,12 +1,10 @@
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import Header from "~/components/Layout/components/Header";
 
 function DefaultLayout({ children }) {
     return (
         <div className="m-7">
             <Header />
             <div className="container m-7">
-                <Sidebar />
                 <div className="content m-7">
                     {children}
                 </div>
